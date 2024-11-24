8646a1eae3e6889b0f9693f8a4868fa2c48412b0
ExecutiveL <124115126+ExecutiveL@users.noreply.github.com>
2024-11-25 01:07:40 +0800

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -5,7 +5,7 @@ import random
 pygame.init()
 
 SUITS = ['C', 'S', 'H', 'D']
-RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9']
 CARD_SIZE = (72, 96)
 CARD_CENTER = (36, 48)
 CARD_BACK_SIZE = (72, 96)
@@ -27,11 +27,11 @@ class Deck:
             for suit in SUITS:
                 self.cards.append((value, suit))
         print(self.cards)
-     
+    #shinuffle ang list by using the rarndomn
     def shuffle(self):
         random.shuffle(self.cards)
         
-
+    #IF yung shinuffle na card ay asa dulo ng list ippop nya
     def deal(self):
         if len(self.cards) > 1:
             return self.cards.pop()
@@ -41,10 +41,10 @@ class Hand(Deck):
         self.cards = []
         self.card_img = []
         self.value = 0 
-
+  
     def add_card(self, card):
         self.cards.append(card)
-
+    #ADDING THE VALUES
     def calc_hand(self):
         first_card_index = [a_card[0] for a_card in self.cards]
         non_aces = [c for c in first_card_index if c != 'A']
